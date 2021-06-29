1d8348d5c3a7e875bc65f02ae117af44a9bb13ca
André Carvalho <andre_carvalho0@live.com>
2021-06-29 16:47:55 -0300

    alter styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,81 +17,78 @@ export default class Header extends Component {
     render() {
         return (
             <>
-                {this.state.nav === true ? (
-                    <header className="header" id="header">
-                        <nav className="nav container">
 
-                            <div className={`nav_menu ${this.state.nav === true ? 'show-menu' : ''}`} id="nav-menu">
-                                <ul className="nav_list grid" onClick={() => this.setState({ nav: false })}>
-                                    <li className="nav_item">
-                                        <a href="#home" className="nav_link">
-                                            <i className={`uil uil-estate nav_icon`} />
-                                            Home
-                                        </a>
-                                    </li>
-                                    <li className="nav_item">
-                                        <a href="#about" className="nav_link">
-                                            <i className={`uil uil-user nav_icon`} />
-                                            About
-                                        </a>
-                                    </li>
-                                    <li className="nav_item">
-                                        <a href="#skills" className="nav_link">
-                                            <i className={`uil uil-file-alt nav_icon`} />
-                                            Skills
-                                        </a>
-                                    </li>
-                                    <li className="nav_item">
-                                        <a href="#services" className="nav_link">
-                                            <i className={`uil uil-bag nav_icon`} />
-                                            Services
-                                        </a>
-                                    </li>
-                                    <li className="nav_item">
-                                        <a href="#portfolio" className="nav_link">
-                                            <i className={`uil uil-scenery nav_icon`} />
-                                            Portfolio
-                                        </a>
-                                    </li>
-                                    <li className="nav_item">
-                                        <a href="#contactme" className="nav_link">
-                                            <i className={`uil uil-message nav_icon`} />
-                                            Contactme
-                                        </a>
-                                    </li>
-                                </ul>
-                                <div onClick={() => this.setState({ nav: false })}>
-                                    <i className="uil uil-times nav_close nav_icon" id="nav-close" />
-                                </div>
+                <header className="header" id="header">
+                    <nav className="nav container">
+                        <div className={`nav_menu ${this.state.nav === true ? 'show-menu' : 'close-menu'}`} id="nav-menu">
+                            <ul className="nav_list grid">
+                                <li className="nav_item">
+                                    <a href="#home" className="nav_link">
+                                        <i className={`uil uil-estate nav_icon`} />
+                                        Home
+                                    </a>
+                                </li>
+                                <li className="nav_item">
+                                    <a href="#about" className="nav_link">
+                                        <i className={`uil uil-user nav_icon`} />
+                                        About
+                                    </a>
+                                </li>
+                                <li className="nav_item">
+                                    <a href="#skills" className="nav_link">
+                                        <i className={`uil uil-file-alt nav_icon`} />
+                                        Skills
+                                    </a>
+                                </li>
+                                <li className="nav_item">
+                                    <a href="#services" className="nav_link">
+                                        <i className={`uil uil-bag nav_icon`} />
+                                        Services
+                                    </a>
+                                </li>
+                                <li className="nav_item">
+                                    <a href="#portfolio" className="nav_link">
+                                        <i className={`uil uil-scenery nav_icon`} />
+                                        Portfolio
+                                    </a>
+                                </li>
+                                <li className="nav_item">
+                                    <a href="#contactme" className="nav_link">
+                                        <i className={`uil uil-message nav_icon`} />
+                                        Contactme
+                                    </a>
+                                </li>
+                            </ul>
+                            <div>
+                                <i  
+                                    className="uil uil-times nav_close nav_icon"
+                                    onClick={() => this.setState({ nav: false })}
+                                />
                             </div>
-                        </nav>
-                    </header>
-                ) : (
-                    <header className="header" id="header">
-                        <nav className="nav container">
-                            <div className={`nav_header ${this.state.nav === true ? 'close-menu' : 'show-menu'}`} id="nav-menu">
-                                <a href="/" className="nav_logo">André</a>
-                                <div className="nav_btns">
-                                    {/* theme change button */}
+                        </div>
+
+                        <div className={`nav_header ${this.state.nav === true ? 'close-menu' : 'show-menu'}`} id="nav-menu">
+                            <a href="/" className="nav_logo">André</a>
+                            <div className="nav_btns">
+                                {/* theme change button */}
+                                <i
+                                    className={`change-theme uil uil-${this.state.theme === 'dark' ? 'sun' : 'moon'}`}
+                                    id="theme-button"
+                                    onClick={(() => {
+                                        this.state.theme === 'dark' ? setTheme('light') : setTheme('dark')
+                                        window.location.reload();
+                                    })}
+                                />
+                                <div className="nav_toggle nav_icon" id="nav-toggle">
                                     <i
-                                        className={`change-theme uil uil-${this.state.theme === 'dark' ? 'sun' : 'sun'}`} 
-                                        id="theme-button"
-                                        onClick={(() => {
-                                            this.state.theme === 'dark' ? setTheme('light') : setTheme('dark')
-                                            window.location.reload();
-                                        })}
+                                        className="uil uil-apps"
+                                        onClick={() => this.setState({ nav: true })}
                                     />
-                                    <div className="nav_toggle nav_icon" id="nav-toggle">
-                                        <i 
-                                            className="uil uil-apps" 
-                                            onClick={() => this.setState({ nav: true })}
-                                        />
-                                    </div>
                                 </div>
                             </div>
-                        </nav>
-                    </header>
-                )}
+                        </div>
+                    </nav>
+                </header>
             </>
         )
     }
