a44159d5cc89e3555d5221e1856a6bddd7aa220a
André Carvalho <andre_carvalho0@live.com>
2023-05-22 21:23:53 -0300

    fix: update year of the experience

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,47 +1,60 @@
-import './styles.css'
-import Image from '../../assets/about.png'
-import Curriculum from '../../assets/CVAndreLeiteCarvalho.pdf';
+import "./styles.css";
+import Image from "../../assets/about.png";
+import Curriculum from "../../assets/CVAndreLeiteCarvalho.pdf";
 
 export default function About() {
-    return (
-        <>
-            <section className="about section" id="about">
-                <h2 className="section_title">About Me</h2>
-                <span className="section_subtitle">My introduction</span>
+  return (
+    <>
+      <section className="about section" id="about">
+        <h2 className="section_title">About Me</h2>
+        <span className="section_subtitle">My introduction</span>
 
-                <div className="about_container container grid">
-                    <img src={Image} alt="profile" className="about_img" />
+        <div className="about_container container grid">
+          <img src={Image} alt="profile" className="about_img" />
 
-                    <div className="about_data">
-                        <p className="about_description">
-                            Web developer, working in web, technologies and Ui / Ux design, delivering quality work.
-                        </p>
+          <div className="about_data">
+            <p className="about_description">
+              Web developer, working in web, technologies and Ui / Ux design,
+              delivering quality work.
+            </p>
 
-                        <div className="about_info">
-                            <div>
-                                <span className="about_info-title">02+</span>
-                                <span className="about_info-name">Years <br /> experience</span>
-                            </div>
+            <div className="about_info">
+              <div>
+                <span className="about_info-title">03+</span>
+                <span className="about_info-name">
+                  Years <br /> experience
+                </span>
+              </div>
 
-                            <div>
-                                <span className="about_info-title">08+</span>
-                                <span className="about_info-name">Completed <br /> project</span>
-                            </div>
+              <div>
+                <span className="about_info-title">08+</span>
+                <span className="about_info-name">
+                  Completed <br /> project
+                </span>
+              </div>
 
-                            <div>
-                                <span className="about_info-title">02+</span>
-                                <span className="about_info-name">Companies <br /> worked</span>
-                            </div>
-                        </div>
+              <div>
+                <span className="about_info-title">02+</span>
+                <span className="about_info-name">
+                  Companies <br /> worked
+                </span>
+              </div>
+            </div>
 
-                        <div className="about_buttons">
-                            <a href={Curriculum} download alt="curriculum" className="button button--flex">
-                                Download CV<i className="uil uil-arrow-to-bottom button_icon"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+            <div className="about_buttons">
+              <a
+                href={Curriculum}
+                download
+                alt="curriculum"
+                className="button button--flex"
+              >
+                Download CV
+                <i className="uil uil-arrow-to-bottom button_icon"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
