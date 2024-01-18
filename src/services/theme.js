598d3a543dcd234d8f42aa5789ac7152bc6cf3af
André Carvalho <andre_carvalho0@live.com>
2024-01-18 19:58:41 -0300

    feature: update images

--- src/services/theme.js
+++ src/services/theme.js
@@ -1,24 +1,19 @@
 import { STORAGE_KEYS } from "../config/storage_keys.config";
 
-export const THEME_KEY = "@airbnb-Token"
+export const THEME_KEY = "@airbnb-Token";
 
 export const getTheme = () => {
-    const theme = localStorage.getItem(STORAGE_KEYS.theme);
+  const theme = localStorage.getItem(STORAGE_KEYS.theme);
 
-    if(theme === null) {
-        return 'light'
-    }
+  if (theme === null) return "light";
 
-    else {
-        return JSON.parse(theme);
-    }
-}
+  return JSON.parse(theme);
+};
 
 export const setTheme = (theme) => {
-    if(theme === null) {
-        localStorage.removeItem(STORAGE_KEYS.theme);
-    }
-    else{
-        localStorage.setItem(STORAGE_KEYS.theme, JSON.stringify(theme))
-    }
-}
+  if (theme === null) {
+    localStorage.removeItem(STORAGE_KEYS.theme);
+  } else {
+    localStorage.setItem(STORAGE_KEYS.theme, JSON.stringify(theme));
+  }
+};
