09c9734436e491907665964e331f33d3a5d0fd6f
carvalhoandre <andre_carvalho0@live.com>
2024-09-30 21:47:14 -0300

    fix: configs

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@config": path.resolve(__dirname, "src/config"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@utils": path.resolve(__dirname, "src/utils"),
+            "@types": path.resolve(__dirname, "src/@types"),
+            "@helper": path.resolve(__dirname, "src/helper"),
+        },
+    },
+});
