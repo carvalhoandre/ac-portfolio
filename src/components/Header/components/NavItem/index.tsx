afcee09163144dae7eea2a1ae502bda492291c8b
carvalhoandre <andre_carvalho0@live.com>
2024-10-03 16:05:26 -0300

    feat: Emphasis

--- src/components/Header/components/NavItem/index.tsx
+++ src/components/Header/components/NavItem/index.tsx
@@ -14,6 +14,7 @@ const NavItem: IComponent<INavITemProps> = ({
     <a href={`#${href}`} className="nav_link" data-testid={testId}>
       <div className="nav_icon_container">
         <Icon icon={icon} className="nav_icon" />
+
         <p className="icon_title">{name || href}</p>
       </div>
     </a>
