eda61471667313b638ca81b20dec75f9c3dd6401
carvalhoandre <andre_carvalho0@live.com>
2024-11-26 11:59:52 -0300

    feat: ac dogs portfolio item

--- src/pages/Home/components/Portfolio/hook.ts
+++ src/pages/Home/components/Portfolio/hook.ts
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import Marvel from "@assets/portfolio/marvel.svg";
 import Store from "@assets/portfolio/store.svg";
 import Countries from "@assets/portfolio/countries.svg";
+import Dogs from "@assets/portfolio/dogs.svg";
 
 import { IUsePortfolios } from "./types";
 
@@ -10,6 +11,12 @@ const usePortfolios = (): IUsePortfolios => {
   const { t } = useTranslation();
 
   const portfolioItems = [
+    {
+      image: Dogs,
+      title: "ac Dogs",
+      description: t("portfolio.dogs"),
+      link: "https://ac-dogs.netlify.app",
+    },
     {
       image: Store,
       title: "ac Store",
